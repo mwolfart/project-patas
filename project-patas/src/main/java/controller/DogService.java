715782d0cccf1d1e3adf9a3b441dce9e491bb269
Jéssica Salvador Rodrigues da Rocha <jessyksrrocha@hotmail.com>
2018-01-04 01:55:16 -0200

    fixes in message, remove link

--- project-patas/src/main/java/controller/DogService.java
+++ project-patas/src/main/java/controller/DogService.java
@@ -25,17 +25,17 @@ public class DogService {
 	@RequestMapping(value = "/dog/register", method = RequestMethod.POST)
 	public ResponseEntity<?> dogRegister(@RequestBody Dog dog) {
 		if(dog.getName() == null)
-			return new ResponseEntity<String>("Nome est� em branco", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("Nome está em branco", HttpStatus.BAD_REQUEST);
 
 		Dog dogWithSameName = dogRepository.findByName(dog.getName());
 		if(dogWithSameName != null)
-			return new ResponseEntity<String>("J� existe um cachorro com este nome", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("Já existe um cachorro com este nome", HttpStatus.BAD_REQUEST);
 
 		if(dog.getArrivalDate() == null)
-			return new ResponseEntity<String>("Data de chegada est� em branco", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("Data de chegada está em branco", HttpStatus.BAD_REQUEST);
 
 		if(dog.getCastrated() == null)
-			return new ResponseEntity<String>("Flag de castrado est� em branco", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("Flag de castrado está em branco", HttpStatus.BAD_REQUEST);
 		
 		//byte[] imgArray  = Helper.saveImage("C:/Users/Jessica/Desktop/cachorro.jpg"); 
 		//Helper.displayImage(imgArray);
@@ -49,17 +49,17 @@ public class DogService {
 	@RequestMapping(value = "/dog/update", method = RequestMethod.POST)
 	public ResponseEntity<String> dogUpdate(@RequestBody Dog dog) {
 		if(dog.getName() == null)
-			return new ResponseEntity<String>("Nome est� em branco", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("Nome está em branco", HttpStatus.BAD_REQUEST);
 
 		Dog dogWithSameName = dogRepository.findByName(dog.getName());
 		if(dogWithSameName != null && dogWithSameName.getId() != dog.getId())
-			return new ResponseEntity<String>("J� existe um cachorro com este nome", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("Já existe um cachorro com este nome", HttpStatus.BAD_REQUEST);
 
 		if(dog.getArrivalDate() == null)
-			return new ResponseEntity<String>("Data de chegada est� em branco", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("Data de chegada está em branco", HttpStatus.BAD_REQUEST);
 
 		if(dog.getCastrated() == null)
-			return new ResponseEntity<String>("Flag de castrado est� em branco", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("Flag de castrado está em branco", HttpStatus.BAD_REQUEST);
 
 		dogRepository.saveAndFlush(dog);
 		return new ResponseEntity<String>(HttpStatus.OK);
@@ -71,7 +71,7 @@ public class DogService {
 		Dog dog = dogRepository.findOne(Long.parseLong(dogId));
 
 		if (dog == null)
-			return new ResponseEntity<String>("Cachorro n�o encontrado.", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("Cachorro não encontrado.", HttpStatus.BAD_REQUEST);
 
 		return new ResponseEntity<Dog>(dog, HttpStatus.OK);
 	}
